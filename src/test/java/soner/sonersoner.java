40747f1304286565ec06dd6be429b35e23318783
sonerkucuk <sonerkucuk5561@gmail.com>
2021-05-19 14:48:34 +0200

    he

--- src/test/java/soner/sonersoner.java
+++ src/test/java/soner/sonersoner.java
@@ -3,5 +3,6 @@ package soner;
 public class sonersoner {
     public static void main(String[] args) {
         System.out.println("herhalde bu olacak");
+        System.out.println("he");
     }
 }
